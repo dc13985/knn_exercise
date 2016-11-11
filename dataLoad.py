0669b9bd180b93d3797ed6584b81ef18d6336647
dc13985 <dc13985@my.bristol.ac.uk>
2016-11-11 14:28:58 +0000

    Update dataLoad.py

--- dataLoad.py
+++ dataLoad.py
@@ -3,6 +3,16 @@ import urllib.request
 import csv
 import random
 
+def source_opt(): 
+    source_choice = "" 
+    while source_choice not in ["y","n"]: 
+        source_choice = input("Did you download the \"iris_flower.txt\" file from the repository (y/n)?\n") 
+        if source_choice.lower() == 'y' or source_choice.lower() == 'n': 
+            return source_choice.lower() 
+        else: 
+            print("\n A valid selection was not made")
+
+
 #import data from web
 def soup_set(location):
     site = location
@@ -12,24 +22,25 @@ def soup_set(location):
     return soup_temp
 
 def struct_soup(soup):
-    dataset = []
-    data_list = soup.get_text("pre")
-    data_list = data_list.split("\n")
-    data_list.pop()
-    data_list.pop()
+    dataset = []
+    data_list = soup.get_text("pre")
+    data_list = data_list.split("\n")
+    data_list.pop()
+    data_list.pop()
+
     for x in range(len(data_list)):
-        temp = data_list[x].split(",")
+        temp = data_list[x].split(",")
         dataset.append(temp)
-    return dataset
+    return dataset
 
 def sort_sets(dataset,split,training_set = [], test_set = []):
-    for x in range(len(dataset)):
-        for y in range(4): # first four entires to float
-            dataset[x][y] = float(dataset[x][y])         
-        if random.random() < split: # split into a training & test set
-            training_set.append(dataset[x])      
-        else:             
-            test_set.append(dataset[x])
+    for x in range(len(dataset)):
+            for y in range(4): # first four entires to float
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split: # split into a training & test set
+                training_set.append(dataset[x])
+            else:
+                test_set.append(dataset[x])
 
 #import data from txt file
 def get_data(filename, split, training_set=[],test_set= []):
@@ -43,3 +54,4 @@ def get_data(filename, split, training_set=[],test_set= []):
                 training_set.append(dataset[x])
             else:
                 test_set.append(dataset[x])
+
